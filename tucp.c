1b59df361a72e07b23919df4fab9d241fc7e5335
DeanRoos <tug91912@temple.edu>
2023-09-19 23:23:05 -0400

    Error handling done

--- tucp.c
+++ tucp.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main (int argc, char *argv[]){
+
+  if (argc < 3){ //dealing with only 1 argument
+		printf("%s", "tucp: incorrect arguments. Exiting program.\n");
+		return 1;
+	}
+
+  //for every possible command, the first argument must be an existing file. 
+  //I'm going to first make sure that this is true and return an error if not.
+  struct stat buf;
+  stat(argv[1], &buf);
+  if (!S_ISREG(buf.st_mode)){
+    puts("tucp: invalid source file name. Exiting program.");
+    return 1;
+  }
+
+}
